57621d176a406f6d06d6679e497b0104c0b03f67
jifukui <805013833@qq.com>
2020-06-05 16:29:03 +0800

    添加新的功能

--- LiguoWeb.c
+++ LiguoWeb.c
@@ -3784,7 +3784,7 @@ uint8 SetDNSName(json_t *json,char *data,char *estr)
 	printf("Hello this is dns\n");
 	if(JsonGetString(json,name))
 	{
-		printf("The name is %s\n",data);
+		printf("The name is %s\n",name);
 		sprintf(str,"#name %s\r",name);
 		printf(str);
 		printf("good set dns\n");
