4e3c2ace45aea68328de8430833ce35bff94d822
jifukui <805013833@qq.com>
2020-06-07 09:54:41 +0800

    调试问题

--- LiguoWeb.c
+++ LiguoWeb.c
@@ -701,7 +701,11 @@ uint8 GetDeviceVideoInfo(char *data,char *estr)
 #endif
 		for(i=0;i<ligPorts;i++)
 		{
-			printf("The port %d have online\n",i);
+			if(portinfo[i]>0)
+			{
+				printf("The port %d have online\n",i);
+			}
+			
 		}
 		while(portinfo[i]>0)
 		{
