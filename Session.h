3d631ab34af7bb2f9706aea84c85a1c1cfa90948
jifukui <805013833@qq.com>
2021-01-26 15:02:51 +0800

    进行模块添加测试

--- Session.h
+++ Session.h
@@ -2,12 +2,12 @@
 #define __SESSION_H_
 #include <openssl/ssl.h>
 struct Session_Management{
-
-}
+    unsigned int temid;
+};
 struct Session_Version{
     unsigned int Major;
     unsigned int Modify;
     unsigned int Id;
-}
+};
 void DisplayVersion();
 #endif
