b522cb11b35a45aefadcb0677537b19160abb0f8
jifukui <805013833@qq.com>
2021-01-26 15:29:03 +0800

    进行模块添加测试

--- Session.c
+++ Session.c
@@ -1,7 +1,7 @@
 #include "Session.h"
 
 
-struct Session_Version Version={1,0,0}
+struct Session_Version Version={1,0,0};
 
 void DisplayVersion(){
     printf("The Session Version is %d.%d.%d\r\n",Version.Major,Version.Modify,Version.Id);
