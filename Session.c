01b978978b0c4376d11693103fec78f28c6ec357
jifukui <805013833@qq.com>
2021-01-28 09:47:03 +0800

    security

--- Session.c
+++ Session.c
@@ -115,7 +115,7 @@ void Display()
 {
     int i = 0 ;
     struct SessionInfo *con;
-    printf("the address is %u\r\n",sessionmanagement);
+    //printf("the address is %u\r\n",sessionmanagement);
     printf("the min is %d max is %d num is %d\r\n",sessionmanagement->min,sessionmanagement->max,sessionmanagement->num);
     for(i;i < SESSION_NUM;i++ ){
         con = &sessionmanagement->sesssion[i] ;
