43efa5d956191e05c2fd2549882e6870d27de11c
jifukui <805013833@qq.com>
2021-01-28 12:54:19 +0800

    security

--- Session.c
+++ Session.c
@@ -130,7 +130,7 @@ void Display()
     }
 }
 int Add(struct ConnectInfo conn){
-    semaphore_wait();
+    //semaphore_wait();
     int i = sessionmanagement->min ;
     struct SessionInfo *con;
     if(sessionmanagement->num>=SESSION_NUM){
@@ -152,7 +152,7 @@ int Add(struct ConnectInfo conn){
             break;
         }
     }
-    semaphore_post();
+    //semaphore_post();
     Display();
     if(i<=SESSION_NUM){
         return i;
@@ -161,7 +161,7 @@ int Add(struct ConnectInfo conn){
     }
 }
 int SetLogStat(unsigned int index,char *str){
-    semaphore_wait();
+    //semaphore_wait();
     struct SessionInfo *con;
     if(!str){
         printf("SetLogStat error Info\r\n");
@@ -186,12 +186,12 @@ int SetLogStat(unsigned int index,char *str){
     }else{
         printf("SetLogStat connected failed\r\n");
     }
-    semaphore_post();
+    //semaphore_post();
     Display();
     return index;
 }
 int Del(unsigned int index){
-    semaphore_wait();
+    //semaphore_wait();
     struct SessionInfo *con;
     if(sessionmanagement->num<=0){
         return 0;
@@ -213,7 +213,7 @@ int Del(unsigned int index){
     }else{
         printf("Del connected failed\r\n");
     }
-    semaphore_post();
+    //semaphore_post();
     Display();
     return index;
 }
