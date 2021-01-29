ac5ae89940561bef6ed424447a3f2fa3c65e5696
jifukui <805013833@qq.com>
2021-01-29 16:38:52 +0800

    good for this

--- Session.c
+++ Session.c
@@ -137,12 +137,12 @@ void Display()
 }
 int Add(struct ConnectInfo conn){
     struct timeval *nowtime = NULL;
-    printf("add\r\n");
-    semaphore_wait();
     int i = sessionmanagement->min ;
     struct SessionInfo *con;
     int time ;
-    struct timeval * t;
+    struct timeval *t = NULL;
+    printf("add\r\n");
+    semaphore_wait();
     if(sessionmanagement->num>=SESSION_NUM){
         return -1;
     }
@@ -158,7 +158,7 @@ int Add(struct ConnectInfo conn){
             if(i>sessionmanagement->max){
                 sessionmanagement->max = i;
             }
-            t = (timeval*) malloc( sizeof(timeval) );
+            t = (struct timeval*) malloc( sizeof(struct timeval) );
             (void) gettimeofday( t, (struct timezone*) 0 );
             time = t->tv_sec*1000000+t->tv_usec;
             printf("have start %u\r\n",time);
@@ -181,7 +181,7 @@ int Add(struct ConnectInfo conn){
 }
 int SetLogStat(unsigned int index,char *str){
     int time ;
-    struct timeval * t;
+    struct timeval * t = NULL ;
     struct timeval *nowtime = NULL;
     printf("SetLogStat\r\n");
     semaphore_wait();
@@ -201,7 +201,7 @@ int SetLogStat(unsigned int index,char *str){
         if(con->timer){
             printf("SetLogStat good for this \r\n");
             tmr_cancel(con->timer);
-            t = (timeval*) malloc( sizeof(timeval) );
+            t = (struct timeval*) malloc( sizeof(ctruct timeval) );
             (void) gettimeofday( t, (struct timezone*) 0 );
             time = t->tv_sec*1000000+t->tv_usec;
             printf("SetLogStat have start %u\r\n",time);
